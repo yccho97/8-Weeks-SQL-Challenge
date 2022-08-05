c1dfa182366cc4ce59472387fca9a4be107c781c
Yi Chien Chong <106817057+yccho97@users.noreply.github.com>
2022-08-05 18:25:49 +0800

    Update Case Study #1 - Danny's Dinner.sql

--- Case Study #1 - Danny's Dinner.sql
+++ Case Study #1 - Danny's Dinner.sql
@@ -4,7 +4,7 @@
 ---- 1. What is the total amount each customer spent at the restaurant?
 SELECT s.customer_id, SUM(price) AS Total_Sales
 FROM dbo.sales as s
-JOIN menu as menu
+	JOIN menu as menu
 		ON s.product_id = menu.product_id
 GROUP BY s.customer_id
 
@@ -32,7 +32,7 @@ GROUP BY customer_id, product_name;
 -- 4. What is the most purchased item on the menu and how many times was it purchased by all customers?
 SELECT TOP 1 sales.product_id, product_name, count (sales.product_id) AS Quantity_Purchased
 FROM sales
-JOIN menu	
+	JOIN menu	
 		ON sales.product_id = menu.product_id
 GROUP BY sales.product_id, menu.product_name
 ORDER BY Quantity_Purchased DESC
@@ -48,7 +48,7 @@ GROUP BY customer_id, product_id
 
 SELECT customer_id, menu.product_name, Quantity_Ordered
 FROM product_rank_cte
-JOIN menu
+	JOIN menu
 		ON product_rank_cte.product_id = menu.product_id
 WHERE rank = 1
 
@@ -58,15 +58,15 @@ WITH mem_sales_cte AS
 SELECT s.customer_id, mem.join_date, s.order_date, s. product_id,
 				DENSE_RANK() OVER(PARTITION BY s.customer_id ORDER BY s.order_date) AS Rank
 FROM sales as s
-		JOIN members as mem
-				ON s.customer_id = mem.customer_id
+	JOIN members as mem
+		ON s.customer_id = mem.customer_id
 WHERE s.order_date >= mem.join_date
 )
 
 SELECT customer_id, join_date, order_date, menu.product_name
 FROM mem_sales_cte
-		JOIN menu 
-					ON mem_sales_cte.product_id = menu.product_id
+	JOIN menu 
+		ON mem_sales_cte.product_id = menu.product_id
 WHERE Rank = 1
 
 -- 7. Which item was purchased just before the customer became a member?
@@ -75,15 +75,15 @@ WITH mem_before_cte AS
 SELECT s.customer_id, s.order_date, s.product_id, mem.join_date, 
 				DENSE_RANK() OVER(PARTITION BY s.customer_id ORDER BY s.order_date DESC) AS RANK
 FROM sales AS s
-		JOIN members AS mem
-				ON s.customer_id = mem.customer_id
+	JOIN members AS mem
+		ON s.customer_id = mem.customer_id
 WHERE s.order_date < mem.join_date
 )
 
 SELECT cte.customer_id, cte.order_date, menu.product_name
 FROM mem_before_cte AS cte
-		JOIN menu AS menu
-					ON	cte.product_id = menu.product_id
+	JOIN menu AS menu
+		ON	cte.product_id = menu.product_id
 WHERE Rank = 1;
 
 -- 8. What is the total items and amount spent for each member before they became a member?
@@ -91,10 +91,10 @@ WITH before_join_cte AS
 (
 SELECT s.customer_id, s.order_date, s.product_id, menu.price, mem.join_date
 FROM sales AS s
-		JOIN members AS mem
-				on s.customer_id = mem.customer_id
-		JOIN menu AS menu
-				ON s.product_id = menu.product_id
+	JOIN members AS mem
+		on s.customer_id = mem.customer_id
+	JOIN menu AS menu
+		ON s.product_id = menu.product_id
 WHERE s.order_date < mem.join_date
 GROUP BY s.customer_id, s.order_date, s.product_id, menu.price, mem.join_date
 )
@@ -117,8 +117,8 @@ CREATE VIEW Points_Table AS
 CREATE VIEW Agg_Points_Table AS
 SELECT customer_id, s.product_id, SUM(pt.points) AS Agg_Points
 FROM sales AS s
-			JOIN Points_Table AS pt 
-						ON pt.product_id = s.product_id
+	JOIN Points_Table AS pt 
+		ON pt.product_id = s.product_id
 GROUP BY customer_id, s.product_id
 
 SELECT customer_id, SUM(Agg_Points) AS Total_Points
@@ -158,10 +158,10 @@ SELECT s.customer_id, s.order_date, m.product_name, m.price,
 						   ELSE 'N'
 						   END AS member
 FROM sales AS s
-			JOIN menu AS m
-						ON s.product_id = m.product_id
-			LEFT JOIN members AS mem
-						ON s.customer_id = mem.customer_id
+	JOIN menu AS m
+		ON s.product_id = m.product_id
+	LEFT JOIN members AS mem
+		ON s.customer_id = mem.customer_id
 
 -- Rank All The Thins - Danny also requires further information about the ranking of customer products, bet he purposely
 -- does not need the ranking for non-member purchases so he expects null ranking values for the records when customers 
@@ -175,14 +175,14 @@ SELECT s.customer_id, s.order_date, m.product_name, m.price,
 						   ELSE 'N'
 						   END AS member
 FROM sales AS s
-			JOIN menu AS m
-						ON s.product_id = m.product_id
-			LEFT JOIN members AS mem
-						ON s.customer_id = mem.customer_id
+	JOIN menu AS m
+		ON s.product_id = m.product_id
+	LEFT JOIN members AS mem
+		ON s.customer_id = mem.customer_id
 )
 
 SELECT *,
-					CASE WHEN member = 'N' THEN NULL
-					ELSE
-								RANK ()	OVER(PARTITION BY customer_id, member ORDER BY order_date) END AS ranking
+	CASE WHEN member = 'N' THEN NULL
+		ELSE
+		RANK ()	OVER(PARTITION BY customer_id, member ORDER BY order_date) END AS ranking
 FROM summary_cte
